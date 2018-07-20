c49b232205ab887621453ff38a674effb90679d7
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-07-20 17:58:35 +0100

    Add exercises 21 to 24. Finish week 1

--- mooc-2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-021.LeapYear/src/LeapYear.java
@@ -9,8 +9,16 @@ public class LeapYear {
         System.out.println("Type a year: ");
 
         int year = Integer.parseInt(reader.nextLine());
-        if ( year % 4 == 0 || ( year % 100 == 0 && year % 400 == 0 ) ) {
-            System.out.println("The year is a leap year");
+        if (year % 4 == 0) {
+            if(year % 100 != 0 ) {
+                System.out.println("The year is a leap year.");
+            } else {
+                if (year % 400 != 0){
+                    System.out.println("The year is not a leap year.");
+                } else {
+                    System.out.println("The year is a leap year.");
+                }
+            }
         } else {
             System.out.println("The year is not a leap year.");
         }
--- mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -6,7 +6,18 @@ public class Password {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
+        String correctMessage = "Right!";
+        String wrongMessage = "Wrong!";
 
-        // Write your code here
+        String str = "";
+        while ( true ) {
+            System.out.print("Type the password: ");
+            str = reader.nextLine();
+            if (str.equals(password)) {
+                System.out.println(correctMessage + "\n\nThe secret is: jryy qbar!");
+                break;
+            }
+            System.out.println(wrongMessage);
+        }
     }
 }
